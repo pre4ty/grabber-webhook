e50bc3ea023b958a30bc10c2a3797a26e9b65b7d
pre4ty <160059137+pre4ty@users.noreply.github.com>
2024-08-28 08:22:58 +0200

    class

---
+++ ipgrabber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cipacipafilip
+{
+    class ipgrabber
+    {
+        public static string GetIP()
+        {
+            string ip = new WebClient().DownloadString("https://api.ipify.org");
+            return ip;
+        }
+    }
+}
